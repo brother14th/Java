0d1c8ca0fdfe91b9f42751af663658646d1a3f7d
ks <hng.khengsiang@gmail.com>
2022-06-05 21:08:46 +0800

    add data

---
+++ Week5/q10/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+    @Autowired
+	private EmployeeData employeeData;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		employeeData.create();
+		employeeData.bulkCreate();
+		System.out.println("data created!");
+	}
+}
